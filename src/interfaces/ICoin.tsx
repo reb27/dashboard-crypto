e2d68059c36a6caf4d1d90db2f519385fd87a00b
reb27 <rebecapedrosa2017@gmail.com>
2023-03-02 13:14:54 -0300

    add: interfaces

---
+++ src/interfaces/ICoin.tsx
@@ -0,0 +1,13 @@
+export interface ICoin {
+    id: number;
+    name: string;
+    symbol: string;
+    current_price: number;
+    total_volume: number;
+    market_cap: number;
+    price_change_percentage_24h: number;
+    image: string;
+    market_cap_rank: number
+}
+
+
