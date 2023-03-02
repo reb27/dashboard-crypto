fa106e43fb336b817f330fd0e3491fecdd7df044
reb27 <rebecapedrosa2017@gmail.com>
2023-03-02 13:22:38 -0300

    add: logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,1 +1,1 @@
-export const Logo = () => <img src="../../assets/logo.svg" width="30px" height="96px" alt="afd" />;
+export const Logo = () => <img src="logo.svg" width="243px" height="58px" alt="afd" />;
